b10c479b643bb007e9c1225e6f2bdc259d5e2adc
AtlasSs01 <86181516+AtlasSs01@users.noreply.github.com>
2023-09-21 19:16:06 +0400

    test

---
+++ Assets/xyy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xyy : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+
+    string hxCheNapisat = "xuyMneVAnus";
+
+    private void Update()
+    {
+        print(hxCheNapisat);
+    }
+}
